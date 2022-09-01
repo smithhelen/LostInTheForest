3f0576255813186de49d7e022f5d19c9cbf5e6bd
HLSmith <h.l.smith@massey.ac.nz>
2022-09-01 12:18:31 +1200

    use ca method as described in the paper rather than inbuilt ranger method

--- methods/ca.R
+++ methods/ca.R
@@ -1,7 +1,7 @@
 #### Prepare test data and training data for ca method ####
 
 # Function to map from variable levels to scores
-standard_ca_map <- function(var, class) {
+factor_to_ordinal <- function(var, class) {
   var_levels <- droplevels(var)
   if (nlevels(var_levels) < 2) {
     # if we only have one var_level we can't do anything
@@ -9,14 +9,14 @@ standard_ca_map <- function(var, class) {
                 extra = list(var_levels=levels(var_levels), num_vars = 1, var_names=NULL, 
                              score = levels(var_levels) %>% as.data.frame() %>% set_names("Var_Level") %>% mutate(Rank = as.numeric(rank(.))))))
   }
-  ct <- table(Var_Level=var_levels,Class=class)
+  ct <- table(Var_Level=var_levels, class=class)
   P <- ct/rowSums(ct)
   S <- cov.wt(P, wt = rowSums(ct))$cov
   pc1 <- eigen(S)$vectors[,1]   ## PCA of weighted covariance matrix of class probabilites
-  score <- P %*% pc1 %>% as.data.frame() %>% rownames_to_column("Var_Level") %>% setNames(., c("Var_Level","PC1"))
-  score2 <- score %>% mutate(Rank = as.numeric(rank(PC1, ties.method = "first"))) %>% select(Var_Level, Rank)
-  var_factor <- factor(var, levels=(score %>% arrange(PC1) %>% pull(Var_Level)), ordered = TRUE, exclude = NULL)
-  list(output = var_factor, extra = list(var_levels=levels(var_levels), num_vars = 1, var_names=NULL, score = score2))
+  score <- P %*% pc1 %>% as.data.frame() %>% rownames_to_column("Var_Level") %>% setNames(., c("Var_Level","PC1")) %>% mutate(Rank = rank(PC1, ties.method = "first"))
+  output_factor <- data.frame(Var_Level = var_levels) %>% left_join(score, by="Var_Level") %>% pull(Rank)
+  list(output = output_factor, 
+       extra = list(score = score, var_levels = levels(var_levels), num_vars = 1, var_names=NULL))
 }
 
 # iterate over the variable columns and grab the output as a new data.frame for ca, and store the absent level stuff for later 
@@ -25,7 +25,7 @@ prepare_training_ca <- function(data, var_cols, class) {
   var_cols <- dplyr::select(data,{{var_cols}})
   classes   <- data %>% pull({{class}})
   # iterate over the variable columns, and convert
-  prepped <- map(var_cols, standard_ca_map, class = classes)
+  prepped <- map(var_cols, factor_to_ordinal, class = classes)
   prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select({{class}}) %>% colnames), 
                             map_dfc(prepped, "output"))
   extra <- map(prepped, "extra")
@@ -33,23 +33,20 @@ prepare_training_ca <- function(data, var_cols, class) {
        extra=extra)
 }
 
-# ranger does all the work for us 
-prepare_test_ca <- function(data) {
-  data
-}
-
 # score absent levels as infinite
-impute_ca <- function(var, extra) {
+impute_ordinal <- function(var, extra) {
   score <- pluck(extra, "score")
+  new_var_level_rank <- score %>% pull(Ranl) %>% max() + 1
   ranked_var <- data.frame(Var_Level = var) %>%
     left_join(score, by="Var_Level") %>%
-    replace_na(list(Rank = Inf))
+    replace_na(list(Rank = new_var_level_rank))
   ranked_var %>% pull(Rank)
 }
 
-# to be used in ca_testing.R
-prepare_test_ca_reimpl <- function(data, extra, id) {
+# iterate over the variable columns and use the extra info from before to remap the levels in the test data. 
+prepare_test_ca <- function(data, extra, id) {
+  # first remap the variable levels to the appropriate ordinal level
   id <- data %>% pull({{id}})
-  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ca)
+  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ordinal)
   newdata_pred <- bind_cols(id=id, newdata_pred)
 }
