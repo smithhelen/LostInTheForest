a1b8cd15f63e109e49050e944041e1279aafc7c1
HLSmith <h.l.smith@massey.ac.nz>
2023-05-18 09:54:20 +1200

    rename to make functions unique to this method

--- methods/ca_unbiased.R
+++ methods/ca_unbiased.R
@@ -4,8 +4,14 @@
 
 # Output is both the score information for the variable (i.e. a vector of the same length as the input vector) and
 # the level mapping data.frame (mapping from var_level to score)
-factor_to_ordinal <- function(var, class) {
+factor_to_ordinal_ca0 <- function(var, class) {
   var_levels <- droplevels(var)
+  if (nlevels(var_levels) < 2) {
+    # if we only have one var_level we can't do anything
+    return(list(output = rep(1, times=length(var)),
+                extra = list(var_levels=levels(var_levels), num_vars = 1, var_names=NULL,
+                             score = data.frame(Var_Level = c(levels(var_levels), "new"), Rank = c(1, 2)))))
+  }
   ct <- table(Var_Level=var_levels,Class=class)
   # add zero row - can't add zeros to ct as P will complain. So add 1/num.classes to P (equal probabilities across classes)
   new <- matrix(rep((1/nlevels(class)),times=nlevels(class)),nrow=1, ncol=nlevels(class), dimnames = list(Var_Level="new",Class=colnames(ct)))
@@ -21,19 +27,19 @@ factor_to_ordinal <- function(var, class) {
 # iterate over the variable columns and grab the output as a new data.frame to send into ca, and store the absent level stuff for later
 prepare_training_ca0 <- function(data, var_cols, class) {
   # pull out our var_cols and class
-  var_cols <- dplyr::select(data,{{var_cols}})
-  classes   <- data %>% pull({{class}})
+  var_cols <- dplyr::select(data, all_of(var_cols))
+  classes   <- data %>% pull(class)
   # iterate over the var columns, and convert
-  prepped <- map(var_cols, factor_to_ordinal, class = classes)
-  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select({{class}}) %>% colnames), 
+  prepped <- map(var_cols, factor_to_ordinal_ca0, class = classes)
+  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select(all_of(class)) %>% colnames), 
                             map_dfc(prepped, "output"))
   extra <- map(prepped, "extra")
   list(training = prepped_data,
        extra = extra)
 }
-
+  
 # given variable information and a level map, do the mapping. Absent levels are mapped to 'new'
-impute_ordinal <- function(var, extra) {
+impute_ordinal_ca0 <- function(var, extra) {
   score <- pluck(extra, "score")
   new_var_level_rank <- score %>% filter(Var_Level == "new") %>% pull(Rank)
   ranked_var <- data.frame(Var_Level = var) %>%
@@ -46,6 +52,8 @@ impute_ordinal <- function(var, extra) {
 prepare_test_ca0 <- function(data, extra, id) {
   # first remap the variable levels to the appropriate ordinal level
   id <- data %>% pull({{id}})
-  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ordinal)
+  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ordinal_ca0)
   newdata_pred <- bind_cols(id=id, newdata_pred)
 }
+
+
