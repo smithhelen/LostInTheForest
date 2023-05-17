c05fb342ea47f7b0b650d408bfb03405bc58ea66
HLSmith <h.l.smith@massey.ac.nz>
2023-05-18 09:53:32 +1200

    Update for different variable types

--- methods/ca.R
+++ methods/ca.R
@@ -1,13 +1,13 @@
 #### Prepare test data and training data for ca method ####
 
 # Function to map from variable levels to scores
-factor_to_ordinal <- function(var, class) {
+factor_to_ordinal_ca <- function(var, class) {
   var_levels <- droplevels(var)
   if (nlevels(var_levels) < 2) {
     # if we only have one var_level we can't do anything
-    return(list(output = var, 
+    return(list(output = rep(1, times=length(var)), 
                 extra = list(var_levels=levels(var_levels), num_vars = 1, var_names=NULL, 
-                             score = levels(var_levels) %>% as.data.frame() %>% set_names("Var_Level") %>% mutate(Rank = as.numeric(rank(.))))))
+                             score = data.frame(Var_Level = levels(var_levels), Rank = 1))))
   }
   ct <- table(Var_Level=var_levels, class=class)
   P <- ct/rowSums(ct)
@@ -22,11 +22,11 @@ factor_to_ordinal <- function(var, class) {
 # iterate over the variable columns and grab the output as a new data.frame for ca, and store the absent level stuff for later 
 prepare_training_ca <- function(data, var_cols, class) {
   # pull out our variable columns and class
-  var_cols <- dplyr::select(data,{{var_cols}})
-  classes   <- data %>% pull({{class}})
+  var_cols <- dplyr::select(data, all_of(var_cols))
+  classes   <- data %>% pull(class)
   # iterate over the variable columns, and convert
-  prepped <- map(var_cols, factor_to_ordinal, class = classes)
-  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select({{class}}) %>% colnames), 
+  prepped <- map(var_cols, factor_to_ordinal_ca, class = classes)
+  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select(all_of(class)) %>% colnames), 
                             map_dfc(prepped, "output"))
   extra <- map(prepped, "extra")
   list(training = prepped_data,
@@ -34,9 +34,9 @@ prepare_training_ca <- function(data, var_cols, class) {
 }
 
 # score absent levels as infinite
-impute_ordinal <- function(var, extra) {
+impute_ordinal_ca <- function(var, extra) {
   score <- pluck(extra, "score")
-  new_var_level_rank <- score %>% pull(Ranl) %>% max() + 1
+  new_var_level_rank <- score %>% pull(Rank) %>% max() + 1
   ranked_var <- data.frame(Var_Level = var) %>%
     left_join(score, by="Var_Level") %>%
     replace_na(list(Rank = new_var_level_rank))
@@ -47,6 +47,6 @@ impute_ordinal <- function(var, extra) {
 prepare_test_ca <- function(data, extra, id) {
   # first remap the variable levels to the appropriate ordinal level
   id <- data %>% pull({{id}})
-  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ordinal)
+  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_ordinal_ca)
   newdata_pred <- bind_cols(id=id, newdata_pred)
 }
