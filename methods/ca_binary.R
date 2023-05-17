fc871c016843b7905becf0ce56b7755ecb6ec6b9
HLSmith <h.l.smith@massey.ac.nz>
2023-05-18 09:53:45 +1200

    Update for different variable types

--- methods/ca_binary.R
+++ methods/ca_binary.R
@@ -8,16 +8,16 @@ binary_ca_map <- function(var, var_name) {
   var_levels <- droplevels(var)
   var_binary <- data.frame(to.dummy(var_levels, var_name))
   list(output = var_binary, 
-       extra = list(var_levels=levels(var_levels), num_vars = 1, var_names=NULL, names = var_name))
+       extra = list(var_levels=levels(var_levels), num_vars=1, var_names=names(var_binary), names = var_name))
 }
 
 prepare_training_ca_binary <- function(data, var_cols, class) {
   # pull out the variable columns and class
-  var_cols <- dplyr::select(data,{{var_cols}})
-  classes   <- data %>% pull({{class}})
+  var_cols <- dplyr::select(data, all_of(var_cols))
+  classes   <- data %>% pull(class)
   # iterate over the variable columns, and convert
   prepped <- map2(var_cols, names(var_cols), binary_ca_map)
-  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select({{class}}) %>% colnames), 
+  prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select(all_of(class)) %>% colnames), 
                             map_dfc(prepped, "output"))
   extra <- map(prepped, "extra")
   list(training = prepped_data,
@@ -27,19 +27,23 @@ prepare_training_ca_binary <- function(data, var_cols, class) {
 # score absent levels as zero
 impute_binary <- function(var, extra) {
   var_name <- pluck(extra, "names")
-  var_levels <- pluck(extra, "var_levels")
-  vars <- paste(var_name, var_levels, sep=".")
-  var_binary <- data.frame(to.dummy(var, var_name)) %>% select(any_of(vars))
-  missing_vars <- setdiff(vars, var_binary %>% colnames())
-  empty_cols <- data.frame(matrix(0, nrow=nrow(var_binary),ncol=length(missing_vars)))
+  var_levels <- pluck(extra, "var_levels") # these are the levels in the training set for the predictor variable
+  train_vars <- extra$var_names # these are the names of the new training binary variables. The will differ from var_levels if there are symbols or spaces!
+  var_binary <- data.frame(to.dummy(var, var_name)) 
+  test_vars <- var_binary %>% select(any_of(train_vars))
+  missing_vars <- setdiff(var_binary %>% colnames(), train_vars)
+  empty_cols <- data.frame(matrix(0, nrow=nrow(test_vars),ncol=length(missing_vars)))
   colnames(empty_cols) <- missing_vars
-  var_binary <- var_binary %>% bind_cols(empty_cols)
+  test_binary <- test_vars %>% bind_cols(empty_cols)
+  test_binary
 }
 
 # iterate over the variable columns and use the extra info from before to remap the levels in the test data
-prepare_test_ca_binary <- function(data, extra, id) {
+prepare_test_ca_binary <- function(data, list_of_extras, id) {
   id <- data %>% pull({{id}})
-  newdata_pred <- map2_dfc(data %>% select(any_of(names(extra))), extra, impute_binary)
+  test_data <- data %>% select(any_of(names(list_of_extras)))
+  newdata_pred <- map2_dfc(test_data, list_of_extras, impute_binary)
   newdata_pred <- bind_cols(id=id, newdata_pred)
+  newdata_pred
 }
 
