6c2a052daf97d8935e6e5ad3a837d68deb3ffe65
HLSmith <h.l.smith@massey.ac.nz>
2023-05-25 17:21:06 +1200

    minor changes

--- methods/pco.R
+++ methods/pco.R
@@ -10,8 +10,8 @@ epsilon <- sqrt(.Machine$double.eps)
 # Output is both the score information for the variable (i.e. a vector of the same length as the input vector) and
 # the level mapping data.frame (mapping from var_level to score)
 factor_to_pco_score <- function(var, dist, axes) {
-  var_levels <- droplevels(var)
-  d.train <- dist[levels(var_levels),levels(var_levels),drop=FALSE]
+  var <- droplevels(var)
+  d.train <- dist[levels(var),levels(var),drop=FALSE]
   A.train <- -0.5 * d.train^2
   B.train <- dbl_center(A.train)
   eigen_B <- eigen_decomp(B.train, symmetric=TRUE)
@@ -27,11 +27,11 @@ factor_to_pco_score <- function(var, dist, axes) {
   lambdas_B <- eigen_B$values[seq_len(nlambdas)]
   Qo <- eigen_B$vectors
   Q <- sweep(Qo[, seq_len(nlambdas), drop=FALSE], 2, sqrt(abs(lambdas_B)), "*")
-  score <- left_join(data.frame(Var_Level = var_levels), data.frame(Q) |> rownames_to_column("Var_Level"), by = "Var_Level")
+  score <- left_join(data.frame(Var_Level = var), data.frame(Q) |> rownames_to_column("Var_Level"), by = "Var_Level")
   output <- score |> select(-Var_Level)
   list(output = output,
-       extra = list(d=dist, var_levels=levels(var_levels), diag.B.train=diag(B.train), Q=Q, lambdas_B=lambdas_B, 
-                    num_vars = ncol(Q), var_names = colnames(Q)))
+       extra = list(d=dist, var_levels=levels(var), diag.B.train=diag(B.train), Q=Q, lambdas_B=lambdas_B, 
+                    dim = ncol(Q), suffix = colnames(Q)))
 }
 
 prepare_training_pco <- function(data, var_cols, class, d, axes=2) {
@@ -43,9 +43,6 @@ prepare_training_pco <- function(data, var_cols, class, d, axes=2) {
   output <- map(prepped,"output")
   prepped_data <- bind_cols(data.frame(classes) %>% setNames(data %>% select(all_of(class)) %>% colnames), 
                             map2(output, names(output), ~ .x %>% set_names(paste(.y, names(.x), sep="."))))
-  
-  
-  
   extra <- map(prepped, "extra")
   list(training = prepped_data,
        extra = extra)
@@ -77,9 +74,9 @@ impute_score_pco <- function(var, extra) {
          select(-Var_Level))
 }
 
-prepare_test_pco <- function(data, extra, id) {
-  var_cols <- data %>% select(any_of(names(extra)))
-  newdata_score <- map2(var_cols, extra, impute_score_pco)
+prepare_test_pco <- function(data, list_of_extras, id) {
+  test_data <- data %>% select(any_of(names(list_of_extras)))
+  newdata_score <- map2(test_data, list_of_extras, impute_score_pco)
   output <- map(newdata_score,"test_score")
   newdata_pred <- map2_dfc(output, names(output), ~ .x %>% set_names(paste(.y, names(.x), sep=".")))
   newdata_pred <- bind_cols(data |> select(all_of(id)), newdata_pred)
