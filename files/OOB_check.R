7495652233102d9b1a93a0038065dd6fc919d254
HLSmith <h.l.smith@massey.ac.nz>
2023-10-27 13:00:30 +1300

    updated plot

--- files/OOB_check.R
+++ files/OOB_check.R
@@ -86,6 +86,27 @@ results |> pivot_longer(cols=c(OOB, misclass), values_to = "error", names_to = "
   facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="Nominal encoding",`TRUE`="Target encoding"))) +
   theme(legend.text = element_text(size=11))
 
+p <- results |> pivot_longer(cols=c(OOB, misclass), values_to = "error", names_to = "method") |> 
+  ggplot(aes(x=k, y=error, colour=method, size=n, shape=method)) + 
+  geom_point() + 
+  #geom_hline(yintercept=1/2, linetype="dotted") +  
+  geom_hline(yintercept=2/3, linetype="dotted") + 
+  theme_bw(base_size = 14)+ 
+  scale_y_continuous("Misclassification rate",expand=c(0,0),limits=c(0,1)) +
+  scale_colour_manual(values=c("#d34728","#193d87"),labels = c("Independent test data", "Out-Of-Bag")) + 
+  scale_shape_manual(values=c(16,18),labels = c("Independent test data", "Out-Of-Bag"))+
+  labs(x="Number of factor levels", colour="Method of calculation", shape="Method of calculation", size="Sample size") + 
+  facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="Target-agnostic encoding",`TRUE`="Target-based encoding"))) +
+  scale_radius(limits = c(0, NA), range = c(1, 5)) + 
+  guides(colour = guide_legend(order = 1, override.aes = list(size = 3)), 
+         shape = guide_legend(order = 1), 
+         size = guide_bins(direction = "vertical"))
+
+pdf("OOB_plot_3classes.pdf", width=7, height=4.5)
+p + theme_bw(base_size = 10.5, base_family = 'serif') +
+  scale_radius(limits = c(0, NA), range = c(1.1, 4)) + 
+  theme(axis.title.x = element_text(margin = ggplot2::margin(t=10)), axis.title.y = element_text(margin = ggplot2::margin(r=5)))
+dev.off()
 
 # What about when there are only two classes (p=2)?
 results_ord <- map_dfr(1:99, function(z) map_dfr(n, function(x) map(k, ~OOB_check(n=x, k=., p=2, ordered=TRUE, method="ranger")))) |>
@@ -93,6 +114,10 @@ results_ord <- map_dfr(1:99, function(z) map_dfr(n, function(x) map(k, ~OOB_chec
 results_unord <- map_dfr(1:99, function(z) map_dfr(n, function(x) map(k, ~OOB_check(n=x, k=., p=2, ordered=FALSE, method="ranger")))) |>
   group_by(n,k) |> summarise(OOB=mean(OOB), misclass=mean(misclass))
 results <- bind_rows("TRUE"=results_ord, "FALSE"=results_unord, .id = "Ordered")
+
+#save(results, file="../Ranger_Data/results/OOB_results_2class.Rdata") # run 20th Sept 2023
+load("../Ranger_Data/results/OOB_results_2class.Rdata")
+
 
 results |> pivot_longer(cols=c(OOB, misclass), values_to = "error", names_to = "method") |>
   ggplot(aes(x=k, y=error, colour=method, size=n)) + geom_point() + 
@@ -102,7 +127,31 @@ results |> pivot_longer(cols=c(OOB, misclass), values_to = "error", names_to = "
   #  facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="respect.unordered.factors=FALSE",`TRUE`="respect.unordered.factors=TRUE"))) +
   #  facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="Alphabetical ordering",`TRUE`="Ordering based on class probabilities"))) +
   facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="Nominal encoding",`TRUE`="Target encoding"))) +
-  theme(legend.text = element_text(size=11))
+  theme(legend.text = element_text(size=11))+
+  theme_bw()
+
+p <- results |> pivot_longer(cols=c(OOB, misclass), values_to = "error", names_to = "method") |> 
+  ggplot(aes(x=k, y=error, colour=method, size=n, shape=method)) + 
+  geom_point() + 
+  geom_hline(yintercept=1/2, linetype="dotted") +  
+  #geom_hline(yintercept=2/3, linetype="dotted") + 
+  theme_bw(base_size = 14)+ 
+  scale_y_continuous("Misclassification rate",expand=c(0,0),limits=c(0,1)) +
+  scale_colour_manual(values=c("#d34728","#193d87"),labels = c("Independent test data", "Out-Of-Bag")) + 
+  scale_shape_manual(values=c(16,18),labels = c("Independent test data", "Out-Of-Bag"))+
+  labs(x="Number of factor levels", colour="Method of calculation", shape="Method of calculation", size="Sample size") + 
+  facet_wrap(~Ordered, labeller = labeller(Ordered=c(`FALSE`="Target-agnostic encoding",`TRUE`="Target-based encoding"))) +
+  scale_radius(limits = c(0, NA), range = c(1, 5)) + 
+  guides(colour = guide_legend(order = 1, override.aes = list(size = 3)), 
+         shape = guide_legend(order = 1), 
+         size = guide_bins(direction = "vertical"))
+
+pdf("OOB_plot_2classes.pdf", width=7, height=4.5)
+p + theme_bw(base_size = 10.5, base_family = 'serif') +
+  scale_radius(limits = c(0, NA), range = c(1.1, 4)) + 
+  theme(axis.title.x = element_text(margin = ggplot2::margin(t=10)), axis.title.y = element_text(margin = ggplot2::margin(r=5)))
+dev.off()
+
 
 
 
