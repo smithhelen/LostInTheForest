e87a0714f21804dff2751255c33db7c50ba570e5
HLSmith <h.l.smith@massey.ac.nz>
2023-11-05 20:29:37 +1300

    aesthetic changes

--- methods/libs_fns.R
+++ methods/libs_fns.R
@@ -6,8 +6,8 @@ library(ranger)
 library(caret)   # for createFolds
 
 # Load in our functions
-source("methods/ca.R") # CA method
-source("methods/binary.R") # Binary method
-source("methods/ca_unbiased.R")   # CA method with new levels scored as zero
-source("methods/pco.R")           # PCO method
-source("methods/similarity.R")        # similarity method
+source("methods/ca.R")                # CA method
+source("methods/binary.R")            # Binary method
+source("methods/ca_unbiased.R")       # CA method with new levels scored as zero
+source("methods/pco.R")               # PCO method
+source("methods/similarity.R")        # Similarity method
