ebeeb37e15bbe2c1f6d11a4dd57c6a4a1c9f9c66
HLSmith <h.l.smith@massey.ac.nz>
2023-11-07 12:21:26 +1300

    Removed hex distances and replaced with hsl and hsv

--- files/colours.R
+++ files/colours.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(stringr)
 library(stringdist)
 library(seqinr) #for col2alpha
+library(DescTools)
+library(farver)
 source("methods/helpers.R") 
 
 pco <- function(d){
@@ -13,17 +15,24 @@ pco <- function(d){
   lambdas_B <- eigen_B$values[1:2]
   Qo <- eigen_B$vectors
   Q <- sweep(Qo[, seq_along(lambdas_B), drop=FALSE], 2, sqrt(abs(lambdas_B)), "*")
-  score <- left_join(colours, data.frame(Q) |> rownames_to_column("Name"), by = "Name")
+  score <- left_join(colours |> select(Name, Hex), data.frame(Q) |> rownames_to_column("Name"), by = "Name")
   score
 }
 
 #############################################################################
 # r colours
+# rgb (red/green/blue); hsv (hue/saturation/value).
 colours <- data.frame(Name=colors()) |> rowwise() |> 
   mutate(Hex=col2alpha(Name),
          R=col2rgb(Name)[1],
          G=col2rgb(Name)[2],
-         B=col2rgb(Name)[3])
+         B=col2rgb(Name)[3]) |> 
+  mutate(HSV.H=ColToHsv(Hex)[1],
+         HSV.S=ColToHsv(Hex)[2],
+         HSV.V=ColToHsv(Hex)[3]) |> 
+  mutate(HSL.H=decode_colour(Name, to="hsl")[1],
+         HSL.S=decode_colour(Name, to="hsl")[2],
+         HSL.L=decode_colour(Name, to="hsl")[3]) |> ungroup()
 
 # check for duplicated names and hex values - both spellings of grey and gray are there
 colours$Name |> table() |> as.data.frame() |> filter(Freq>1)
@@ -31,33 +40,37 @@ colours$Hex |> table() |> as.data.frame() |> filter(Freq>1)
 
 colours <- colours |> distinct(Hex, .keep_all = TRUE)
 
-# calculate distance matrices based on name, hex name, and rgb values
+# calculate distance matrices based on name, rgb, and hsv values
 d.name <- stringdistmatrix(colours$Name, colours$Name, method = "lv", useNames = "names") 
 colnames(d.name) <- colours$Name
 rownames(d.name) <- colours$Name
-
-d.hex <- stringdistmatrix(colours$Hex, colours$Hex, method = "lv", useNames = "names") 
-colnames(d.hex) <- colours$Name
-rownames(d.hex) <- colours$Name
 
 d.rgb <- colours |> select(c("R","G","B")) |> dist(diag=TRUE, upper=TRUE) |> as.matrix()
 colnames(d.rgb) <- colours$Name
 rownames(d.rgb) <- colours$Name
 
+d.hsv <- colours |> select(c("HSV.H","HSV.S","HSV.V")) |> dist(diag=TRUE, upper=TRUE) |> as.matrix()
+colnames(d.hsv) <- colours$Name
+rownames(d.hsv) <- colours$Name
+
+d.hsl <- colours |> select(c("HSL.H","HSL.S","HSL.L")) |> dist(diag=TRUE, upper=TRUE) |> as.matrix()
+colnames(d.hsl) <- colours$Name
+rownames(d.hsl) <- colours$Name
+
 # perform pco analysis
-pco.name <- pco(d.name) |> rename(PCO1 = V1, PCO2 = V2)
-pco.hex <- pco(d.hex) |> rename(PCO1 = V1, PCO2 = V2)
-pco.rgb <- pco(d.rgb) |> rename(PCO1 = V1, PCO2 = V2)
+pco.name <- pco(d.name) |> rename(PCO1.name = V1, PCO2.name = V2)
+pco.rgb <- pco(d.rgb) |> rename(PCO1.rgb = V1, PCO2.rgb = V2)
+pco.hsv <- pco(d.hsv) |> rename(PCO1.hsv = V1, PCO2.hsv = V2)
+pco.hsl <- pco(d.hsl) |> rename(PCO1.hsl = V1, PCO2.hsl = V2)
 
 
 # plot 1d and 2d
-pco.plot <- list(pco.name, pco.hex, pco.rgb) |> 
-  reduce(right_join, by=c("Name","Hex","R","G","B"), suffix = c(".name",".hex")) |> 
-  rename(PCO1.rgb = PCO1, PCO2.rgb = PCO2) |> 
+pco.plot <- list(pco.name, pco.rgb, pco.hsv, pco.hsl) |> 
+  reduce(left_join, by = c("Name","Hex")) |>  
   pivot_longer(cols = c(starts_with("PCO1"), starts_with("PCO2")), 
                names_to = c(".value", "distance"), 
                names_sep = "\\.") |> 
-  mutate(distance = factor(distance, levels=c("name","hex","rgb")))
+  mutate(distance = factor(distance, levels=c("name","rgb", "hsv", "hsl")))
 
 pco.plot |> ggplot(aes(x=PCO1, y=0, group=distance, colour=Name)) + 
   geom_point(size=2, pch=19) +
@@ -67,10 +80,11 @@ pco.plot |> ggplot(aes(x=PCO1, y=0, group=distance, colour=Name)) +
         axis.ticks.y = element_blank(),
         axis.title.y = element_blank(),
       legend.position = "none") +
-  facet_wrap(~distance, nrow = 3, scales="free",
+  facet_wrap(~distance, nrow = 4, scales="free",
              labeller = labeller(distance=c(name = "Distance from colour names",
-                                            hex = "Distance from Hex values",
-                                            rgb = "Distance from RGB values"))) 
+                                            rgb = "Distance from RGB values",
+                                            hsv = "Distance from HSV values",
+                                            hsl = "Distance from HSL values"))) 
 
 pco.plot |> ggplot(aes(x=PCO1, y=PCO2, group=distance, colour=Name)) + 
   geom_point(size=2, pch=19) +
@@ -80,7 +94,8 @@ pco.plot |> ggplot(aes(x=PCO1, y=PCO2, group=distance, colour=Name)) +
         legend.position = "none") +
   facet_wrap(~distance, nrow = 1, scales="free",
              labeller = labeller(distance=c(name = "Distance from colour names",
-                                            hex = "Distance from Hex values",
-                                            rgb = "Distance from RGB values"))) 
+                                            rgb = "Distance from RGB values",
+                                            hsv = "Distance from HSV values",
+                                            hsl = "Distance from HSL values"))) 
 
 ###################################################################################################)
