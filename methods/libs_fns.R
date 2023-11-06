cb40063e56164511f51952c17b2df0e9df0a41ae
HLSmith <h.l.smith@massey.ac.nz>
2023-11-07 12:22:08 +1300

    updated the libraries for lost in the forest methods

--- methods/libs_fns.R
+++ methods/libs_fns.R
@@ -3,11 +3,16 @@
 # Load libraries
 library(tidyverse)
 library(ranger)
-library(caret)   # for createFolds
+library(recipes)
+library(rsample) # for initial_split()
+library(tidymodels)
+#library(finetune)
+#library(caret)   # for createFolds
 
 # Load in our functions
 source("methods/ca.R")                # CA method
-source("methods/binary.R")            # Binary method
 source("methods/ca_unbiased.R")       # CA method with new levels scored as zero
 source("methods/pco.R")               # PCO method
-source("methods/similarity.R")        # Similarity method
+source('methods/recipe_ca.R')         # CA recipe
+source('methods/recipe_ca0.R')        # CA unbiased recipe
+source("methods/recipe_pco.R")        # PCO recipe
