6e24217c48c4b17eca40b2c58630d060410440ee
HLSmith <h.l.smith@massey.ac.nz>
2023-11-29 12:55:50 +1300

    updated name for consistency

--- methods/libs_fns.R
+++ methods/libs_fns.R
@@ -11,7 +11,7 @@ library(tidymodels)
 
 # Load in our functions
 source("methods/ca.R")                # CA method
-source("methods/ca_unbiased.R")       # CA method with new levels scored as zero
+source("methods/ca0.R")       # CA method with new levels scored as zero
 source("methods/pco.R")               # PCO method
 source('methods/recipe_ca.R')         # CA recipe
 source('methods/recipe_ca0.R')        # CA unbiased recipe
